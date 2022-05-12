ffce0eb0803bb77967f923e0e6c265be26fe2eab
Idealist226 <1641955581@qq.com>
2022-05-12 12:44:36 +0800

    init3

--- myOS/c5/c/WriteKernelToDisk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Author:gzh
-
-gcc -m32 -c -o ./kernel/main.o ./kernel/main.c && \
-ld ./kernel/main.o -N -Ttext=0xc0001500 -e main -m elf_i386 -o ./kernel/kernel.bin && \
-strip --remove-section=.note.gnu.property ./kernel/kernel.bin && \
-dd if=./kernel/kernel.bin of=/usr/share/bochs/hd60M.img bs=512 count=200 seek=9 conv=notrunc
--- myOS/c6/b/kernel/main.c
+++
@@ -1,5 +0,0 @@
-#include "print.h"
-void main(){
-    put_str("I am kernel\n");
-    while(1);
-}
